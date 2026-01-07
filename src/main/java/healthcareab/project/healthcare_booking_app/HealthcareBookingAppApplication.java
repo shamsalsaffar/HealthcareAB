95d3d690411d0df8df3f0bd6ffdd598e08b54bc1
Iasmina Alkashash <iasminakash@hotmail.com>
2026-01-08 00:37:34 +0100

    remove logs

--- src/main/java/healthcareab/project/healthcare_booking_app/HealthcareBookingAppApplication.java
+++ src/main/java/healthcareab/project/healthcare_booking_app/HealthcareBookingAppApplication.java
@@ -8,9 +8,6 @@ public class HealthcareBookingAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HealthcareBookingAppApplication.class, args);
-		System.out.println("DB_URL: " + System.getenv("DB_URL"));
-		System.out.println("DB_USERNAME: " + System.getenv("DB_USERNAME"));
-		System.out.println("DB_PASSWORD: " + System.getenv("DB_PASSWORD"));
 	}
 
 }
