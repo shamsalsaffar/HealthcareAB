a2173cf8136a49c96a41779aea3d3e5e13d9376f
Iasmina Alkashash <iasminakash@hotmail.com>
2026-01-08 00:17:01 +0100

    add log lines

--- src/test/java/healthcareab/project/healthcare_booking_app/HealthcareBookingAppApplicationTests.java
+++ src/test/java/healthcareab/project/healthcare_booking_app/HealthcareBookingAppApplicationTests.java
@@ -1,6 +1,7 @@
 package healthcareab.project.healthcare_booking_app;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
@@ -8,8 +9,21 @@ import org.springframework.test.context.ActiveProfiles;
 @SpringBootTest
 class HealthcareBookingAppApplicationTests {
 
+
+	@Value("${DB_URL}")
+	private String dbUrl;
+
+	@Value("${DB_USERNAME}")
+	private String dbUsername;
+
+	@Value("${DB_PASSWORD}")
+	private String dbPassword;
+
 	@Test
 	void contextLoads() {
+		System.out.println("DB_URL: " + dbUrl);
+		System.out.println("DB_USERNAME: " + dbUsername);
+		System.out.println("DB_PASSWORD: " + dbPassword);
 	}
 
 }
