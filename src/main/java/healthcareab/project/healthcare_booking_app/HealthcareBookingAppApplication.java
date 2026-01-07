5d17fe1fdaec3965d7ebf23a31d9b63e70650055
Iasmina Alkashash <iasminakash@hotmail.com>
2026-01-08 00:18:46 +0100

    add log lines

--- src/main/java/healthcareab/project/healthcare_booking_app/HealthcareBookingAppApplication.java
+++ src/main/java/healthcareab/project/healthcare_booking_app/HealthcareBookingAppApplication.java
@@ -1,13 +1,27 @@
 package healthcareab.project.healthcare_booking_app;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HealthcareBookingAppApplication {
 
+
+	@Value("${DB_URL}")
+	private String dbUrl;
+
+	@Value("${DB_USERNAME}")
+	private String dbUsername;
+
+	@Value("${DB_PASSWORD}")
+	private String dbPassword;
+
 	public static void main(String[] args) {
 		SpringApplication.run(HealthcareBookingAppApplication.class, args);
+		System.out.println("DB_URL: " + dbUrl);
+		System.out.println("DB_USERNAME: " + dbUsername);
+		System.out.println("DB_PASSWORD: " + dbPassword);
 	}
 
 }
--- src/test/java/healthcareab/project/healthcare_booking_app/HealthcareBookingAppApplicationTests.java
+++ src/test/java/healthcareab/project/healthcare_booking_app/HealthcareBookingAppApplicationTests.java
@@ -1,7 +1,6 @@
 package healthcareab.project.healthcare_booking_app;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
@@ -9,21 +8,8 @@ import org.springframework.test.context.ActiveProfiles;
 @SpringBootTest
 class HealthcareBookingAppApplicationTests {
 
-
-	@Value("${DB_URL}")
-	private String dbUrl;
-
-	@Value("${DB_USERNAME}")
-	private String dbUsername;
-
-	@Value("${DB_PASSWORD}")
-	private String dbPassword;
-
 	@Test
 	void contextLoads() {
-		System.out.println("DB_URL: " + dbUrl);
-		System.out.println("DB_USERNAME: " + dbUsername);
-		System.out.println("DB_PASSWORD: " + dbPassword);
 	}
 
 }
