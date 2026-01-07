e254c540664556d2134d49dff71e5b92870f7f0d
Iasmina Alkashash <iasminakash@hotmail.com>
2026-01-08 00:12:05 +0100

    configure test profile application properties

--- src/test/java/healthcareab/project/healthcare_booking_app/HealthcareBookingAppApplicationTests.java
+++ src/test/java/healthcareab/project/healthcare_booking_app/HealthcareBookingAppApplicationTests.java
@@ -2,7 +2,9 @@ package healthcareab.project.healthcare_booking_app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class HealthcareBookingAppApplicationTests {
 
