067be92481d1fa32b5b5ba56efb5ceccd460ebeb
Iasmina Alkashash <iasminakash@hotmail.com>
2026-01-08 00:21:00 +0100

    add log lines

--- src/main/java/healthcareab/project/healthcare_booking_app/HealthcareBookingAppApplication.java
+++ src/main/java/healthcareab/project/healthcare_booking_app/HealthcareBookingAppApplication.java
@@ -1,27 +1,16 @@
 package healthcareab.project.healthcare_booking_app;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HealthcareBookingAppApplication {
 
-
-	@Value("${DB_URL}")
-	private String dbUrl;
-
-	@Value("${DB_USERNAME}")
-	private String dbUsername;
-
-	@Value("${DB_PASSWORD}")
-	private String dbPassword;
-
 	public static void main(String[] args) {
 		SpringApplication.run(HealthcareBookingAppApplication.class, args);
-		System.out.println("DB_URL: " + dbUrl);
-		System.out.println("DB_USERNAME: " + dbUsername);
-		System.out.println("DB_PASSWORD: " + dbPassword);
+		System.out.println("DB_URL: " + System.getenv("DB_URL"));
+		System.out.println("DB_USERNAME: " + System.getenv("DB_USERNAME"));
+		System.out.println("DB_PASSWORD: " + System.getenv("DB_PASSWORD"));
 	}
 
 }
