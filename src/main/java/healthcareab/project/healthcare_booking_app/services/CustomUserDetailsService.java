bffebc385e66b5150816e14ae225ae2a9f6c8afb
Iasmina Alkashash <iasminakash@hotmail.com>
2026-01-12 23:08:26 +0100

    made formatting updated after pipe line formatting failure

--- src/main/java/healthcareab/project/healthcare_booking_app/services/CustomUserDetailsService.java
+++ src/main/java/healthcareab/project/healthcare_booking_app/services/CustomUserDetailsService.java
@@ -24,7 +24,6 @@ public class CustomUserDetailsService implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found"));
-
         return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(),
                 getAuthorities(user));
     }
