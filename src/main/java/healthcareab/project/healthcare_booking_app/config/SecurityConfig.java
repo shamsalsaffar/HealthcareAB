47d0bdacdd7946f016ab5141ddc88490c8d587f3
Shams Alsaffar <shamsay_1985@yahoo.com>
2026-01-13 23:41:25 +0100

    Resolve checkstyle

--- src/main/java/healthcareab/project/healthcare_booking_app/config/SecurityConfig.java
+++ src/main/java/healthcareab/project/healthcare_booking_app/config/SecurityConfig.java
@@ -4,7 +4,6 @@ import healthcareab.project.healthcare_booking_app.filters.JwtAuthenticationFilt
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
