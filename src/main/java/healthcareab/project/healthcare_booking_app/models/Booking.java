126aa6e6d5ff0eea86518f508150adc66922de69
Patrik Turemark <p.turemark@gmail.com>
2026-01-13 10:28:45 +0100

    fixing pipline error

--- src/main/java/healthcareab/project/healthcare_booking_app/models/Booking.java
+++ src/main/java/healthcareab/project/healthcare_booking_app/models/Booking.java
@@ -1,7 +1,18 @@
 package healthcareab.project.healthcare_booking_app.models;
 
 import healthcareab.project.healthcare_booking_app.models.enums.BookingStatus;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
