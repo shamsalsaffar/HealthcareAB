1b3d642b8436b47a2b9e39a377fe8ddb37a4c48d
Patrik Turemark <p.turemark@gmail.com>
2026-01-13 10:39:38 +0100

    removed unnessesary annotation

--- src/main/java/healthcareab/project/healthcare_booking_app/models/Booking.java
+++ src/main/java/healthcareab/project/healthcare_booking_app/models/Booking.java
@@ -1,24 +1,12 @@
 package healthcareab.project.healthcare_booking_app.models;
 
 import healthcareab.project.healthcare_booking_app.models.enums.BookingStatus;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Inheritance;
-import jakarta.persistence.InheritanceType;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
 
 @Entity
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @Table(name = "bookings")
 public class Booking {
 
