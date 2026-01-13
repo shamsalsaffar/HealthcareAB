729f7d6ee33f00a14aafc51ad31d1687ecd722b3
Patrik Turemark <p.turemark@gmail.com>
2026-01-13 11:21:36 +0100

    created booking repository and createBooking methods

--- src/main/java/healthcareab/project/healthcare_booking_app/controllers/BookingController.java
+++ src/main/java/healthcareab/project/healthcare_booking_app/controllers/BookingController.java
@@ -1,4 +1,16 @@
 package healthcareab.project.healthcare_booking_app.controllers;
 
+import healthcareab.project.healthcare_booking_app.models.Booking;
+import healthcareab.project.healthcare_booking_app.services.BookingService;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class BookingController {
+
+    private BookingService bookingService;
+
+    public Booking createBooking(Booking booking){
+
+        return bookingService.createBooking(booking);
+    }
 }
--- src/main/java/healthcareab/project/healthcare_booking_app/services/BookingService.java
+++ src/main/java/healthcareab/project/healthcare_booking_app/services/BookingService.java
@@ -1,4 +1,22 @@
 package healthcareab.project.healthcare_booking_app.services;
 
+import healthcareab.project.healthcare_booking_app.models.Booking;
+import healthcareab.project.healthcare_booking_app.repository.BookingRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class BookingService {
+
+    private final BookingRepository bookingRepository;
+
+    public BookingService(BookingRepository bookingRepository) {
+        this.bookingRepository = bookingRepository;
+    }
+
+    public Booking createBooking (Booking booking) {
+
+
+
+        return bookingRepository.save(booking);
+    }
 }
