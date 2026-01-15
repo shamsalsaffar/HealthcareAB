675e5ef9628a8c5dfb0bad22762cab36d89de76b
denlillamurmeln <rooba87@gmail.com>
2026-01-15 14:31:29 +0100

    created a AvailabilityServiceTest class

---
+++ src/test/java/healthcareab/project/healthcare_booking_app/services/AvailabilityServiceTest.java
@@ -0,0 +1,62 @@
+package healthcareab.project.healthcare_booking_app.services;
+
+import org.junit.jupiter.api.Test;
+
+public class AvailabilityServiceTest {
+
+    @Test
+    void whenCreateAvailability_shouldReturnSuccess() {
+        //Arrange
+
+        //Act
+
+        //Assert
+    }
+
+    @Test
+    void whenUpdateAvailability_shouldReturnSuccess() {
+        //Arrange
+
+        //Act
+
+        //Assert
+    }
+
+    @Test
+    void whenDeleteAvailability_shouldReturnSuccess() {
+        //Arrange
+
+        //Act
+
+        //Assert
+    }
+
+    @Test
+    void whenGetAvailabilityById_shouldReturnSuccess() {
+        //Arrange
+
+        //Act
+
+        //Assert
+    }
+
+    @Test
+    void whenGetAllAvailability_shouldReturnSuccess() {
+        //Arrange
+
+        //Act
+
+        //Assert
+    }
+
+    @Test
+    void whenCreateAvailability_shouldReturnCaregiverNotFound() {
+        //Arrange
+
+        //Act
+
+        //Assert
+    }
+
+
+}
