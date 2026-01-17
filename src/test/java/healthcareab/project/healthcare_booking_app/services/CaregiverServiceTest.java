092feb7ad5c4bab7bb18b71f00eb3a86066ced2d
Iasmina Alkashash <iasminakash@hotmail.com>
2026-01-17 22:44:28 +0100

    eliminating multiple imports

--- src/test/java/healthcareab/project/healthcare_booking_app/services/CaregiverServiceTest.java
+++ src/test/java/healthcareab/project/healthcare_booking_app/services/CaregiverServiceTest.java
@@ -15,8 +15,14 @@ import org.mockito.MockitoAnnotations;
 import java.util.NoSuchElementException;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 class CaregiverServiceTest {
 
