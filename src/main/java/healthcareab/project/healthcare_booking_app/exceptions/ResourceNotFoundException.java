5662303080b17a256814b222f64124fa00c49f76
Iasmina Alkashash <iasminakash@hotmail.com>
2026-01-17 23:15:01 +0100

    updating projekt efter pull comflict error

--- src/main/java/healthcareab/project/healthcare_booking_app/exceptions/ResourceNotFoundException.java
+++ src/main/java/healthcareab/project/healthcare_booking_app/exceptions/ResourceNotFoundException.java
@@ -1,4 +1,4 @@
-package healthcareab.project.healthcare_booking_app.exception;
+package healthcareab.project.healthcare_booking_app.exceptions;
 
 public class ResourceNotFoundException extends RuntimeException {
     public ResourceNotFoundException(String message) {
