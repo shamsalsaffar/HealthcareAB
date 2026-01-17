19a46cf9db5133daa80989c8b06005597932101c
Iasmina Alkashash <iasminakash@hotmail.com>
2026-01-17 22:22:57 +0100

    add missing import

--- src/test/java/healthcareab/project/healthcare_booking_app/HealthcareBookingAppApplicationTests.java
+++ src/test/java/healthcareab/project/healthcare_booking_app/HealthcareBookingAppApplicationTests.java
@@ -1,5 +1,6 @@
 package healthcareab.project.healthcare_booking_app;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
